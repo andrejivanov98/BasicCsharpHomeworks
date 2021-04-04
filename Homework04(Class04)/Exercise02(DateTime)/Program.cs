f8a37dcf6b8fb9f49ae1f7a76a13e6f1cced33e5
itsandrejivanov <ivanovandrej1995@gmail.com>
2021-04-04 23:26:42 +0200

    DateTime homework added

---
+++ Homework04(Class04)/Exercise02(DateTime)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise02_DateTime_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("When is your birthday?");
+            string birthday = Console.ReadLine();
+            DateTime birthDate = DateTime.Parse(birthday);
+            AgeCalculator(birthDate);
+        }
+
+        static void AgeCalculator(DateTime birthDate)
+        {
+            int age = 0;
+            age = DateTime.Now.Year - birthDate.Year;
+            if (DateTime.Now.DayOfYear < birthDate.DayOfYear)
+                age = age - 1;
+
+            Console.WriteLine($"You are {age} years old.");
+
+        }
+
+    }
+}

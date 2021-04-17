2cbd4b6818198b3c617a05812fbe083f8ca93a45
itsandrejivanov <ivanovandrej1995@gmail.com>
2021-04-17 09:04:24 +0200

    Collections homework added

---
+++ Homework07(Class08)/Task01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>();
+            Queue<int> myQueue = new Queue<int>();
+            Stack<int> myStack = new Stack<int>();
+
+            do
+            {
+                int num;
+                while (true)
+                {
+                    Console.WriteLine("Insert a number");
+                    if (int.TryParse(Console.ReadLine(), out num)) { break; }
+                }
+                myQueue.Enqueue(num);
+                myStack.Push(num);
+                myList.Add(num);
+                Console.WriteLine("Do you want to insert another number? Y/N");
+            }
+            
+            while (Console.ReadLine().ToUpper() == "Y");
+            PrintCollections(myList);
+            PrintCollections(myQueue);
+            PrintCollections(myStack);
+        }
+
+        static void PrintCollections(IEnumerable collection)
+        {
+            foreach (object x in collection)
+            {
+                Console.WriteLine($"{collection} : {x}");
+            }
+        }
+    }
+}
+
+        
